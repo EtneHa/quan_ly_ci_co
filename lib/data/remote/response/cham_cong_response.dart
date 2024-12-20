9cb3e165acd8faf398a5dde0fec577db2b94ae2b
HaNgo <halatao23vn123@gmail.com>
2024-12-20 22:33:11 +0700

    asdfas

--- lib/data/remote/response/cham_cong_response.dart
+++ lib/data/remote/response/cham_cong_response.dart
@@ -1,11 +1,104 @@
 import 'package:quan_ly_ci_co/data/remote/response/base_response.dart';
+import 'package:quan_ly_ci_co/domain/models/bangcong_model.dart';
 
-class ChamCongResponse extends BaseResponse{
+class ChamCongResponse extends BaseResponse {
   ChamCongResponse({required super.success, required super.message});
   factory ChamCongResponse.fromJson(Map<String, dynamic> json) {
     return ChamCongResponse(
       success: json['success'],
       message: json['message'],
+    );
+  }
+}
+
+class ListChamCongResponse extends BaseResponse {
+  ListChamCongResponse(this.data,
+      {required super.success, required super.message, this.totalCount});
+  final List<ChamCongNhanVien> data;
+  final int? totalCount;
+
+  factory ListChamCongResponse.fromJson(Map<String, dynamic> json) {
+    return ListChamCongResponse(
+      (json['data'] as List)
+          .map((e) => ChamCongNhanVien.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      success: json['success'] as bool,
+      message: json['message'] as String,
+      totalCount: json['totalCount'] as int,
+    );
+  }
+}
+
+class ChamCongNhanVien {
+  final String? id_nhanvien;
+  final String? ten;
+  final String? phongban;
+  final List<ChamCong>? chamCong;
+
+  ChamCongNhanVien(
+      {required this.id_nhanvien,
+      required this.ten,
+      required this.phongban,
+      required this.chamCong});
+
+  factory ChamCongNhanVien.fromJson(Map<String, dynamic> json) {
+    return ChamCongNhanVien(
+      id_nhanvien: json['id_nhanvien'] as String,
+      ten: json['ten'] as String,
+      phongban: json['phongban'] as String,
+      chamCong: (json['chamCong'] as List)
+          .map((e) => ChamCong.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id_nhanvien': id_nhanvien,
+      'ten': ten,
+      'phongban': phongban,
+      'chamCong': chamCong?.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  BangCongModel toBangCongModel() {
+    return BangCongModel(
+      id: id_nhanvien ?? '',
+      name: ten ?? '',
+      department: phongban ?? '',
+      chamCong: chamCong?.map((e) => e.toChamCongModel()).toList() ?? [],
+    );
+  }
+}
+
+class ChamCong {
+  final String? ngay;
+  final String? giovao;
+  final String? giora;
+
+  ChamCong({required this.ngay, required this.giovao, required this.giora});
+
+  factory ChamCong.fromJson(Map<String, dynamic> json) {
+    return ChamCong(
+      ngay: json['ngay'] as String,
+      giovao: json['giovao'] as String,
+      giora: json['giora'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'ngay': ngay,
+      'giovao': giovao,
+      'giora': giora,
+    };
+  }
+
+  ChamCongModel toChamCongModel() {
+    return ChamCongModel(
+      label: ngay ?? '',
+      checkIn: giovao,
+      checkOut: giora,
     );
   }
 }
